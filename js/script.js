c9076c76f739b9cd62e99160951ded5c57c225ea
GUSTAVO <cg3026701@LAB202M26.IFSP>
2023-09-20 10:30:52 -0300

    jogo da velha finalizado

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//contador de palavras
+
+let player1 = 0;
+let player2 = 0;
+
+let x = document.querySelector('.X');
+let o = document.querySelector('.o');
+let boxes = document.querySelectorAll('.box');
+let buttons = document.querySelectorAll('#buttons-container button');
+let messageContainer = document.querySelector('#message');
+let messageText = document.querySelector('#message p');
+let secondPlayer;
